3522e100829378f4a6919c9506c5cea24f41aad0
Silari <Silari@users.noreply.github.com>
2020-05-02 03:26:17 -0400

    Update apitoken.py
    Set the twitchheader to automatically be set up if the client ID is provided.

--- apitoken.py
+++ apitoken.py
@@ -1,6 +1,6 @@
 #Discord token for bot account
 token = ''
 #twitch header with token, client ID, and client secret, all needed for OAuth which is now required.
-twitchheader = ''#{"Client-ID": "<yourtokenhere>"}
 clientid = '' #Your twitch client ID
 clientsecret = '' #Your twitch client secret
+twitchheader = {"Client-ID": clientid}
