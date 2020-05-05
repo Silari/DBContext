3b20fd74c8b1eadf88d7d6412b41cac3567b1319
Silari <Silari@users.noreply.github.com>
2020-05-05 04:28:47 -0400

    Update apitoken.py

--- apitoken.py
+++ apitoken.py
@@ -1,12 +1,9 @@
 #Discord token for bot account
-token = 'NTUzMzM1Mjc3NzA0NDQ1OTUz.D2Mlvg.e8F3u1hWfiQUS_42J1fwWLrtXBs'
+token = ''
 #Dev account
-token = 'NTU1MDYyMDE5OTY4MjA0ODAw.D2ltzQ.AsejpPdJ9Bqw0Qk5mRn89h4mOs8'
+token = ''
 #New stuff for twitch cause 434580345 complications were needed I guess
-clientid = 'zz5jbpm0b775e6kqnp5t04i51xo44v'
-clientsecret = 'eqbyva82hjhvg8wowb2r0dm9gsp2g9'
+clientid = ''
+clientsecret = ''
 #twitch header with token
 twitchheader = {"Client-ID": clientid}
-#Invite for the development version of picartobot. Not open to the public.
-devinvite = "https://discordapp.com/api/oauth2/authorize?client_id=555062019968204800&scope=bot&permissions=478272"
-devroleinvite = "https://discordapp.com/api/oauth2/authorize?client_id=555062019968204800&scope=bot&permissions=268913728"
