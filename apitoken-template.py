0a824ce303ecd3605e0078d6e40fabe4e5a843eb
Silari <Silari@users.noreply.github.com>
2020-08-02 19:31:10 -0400

    Update and rename apitoken.py to apitoken-template.py
    Renamed and added verbage to describe how to use the file. apitoken.py is included in the .gitignore so it shouldn't be uploaded on accident. Anymore.

---
+++ apitoken-template.py
@@ -0,0 +1,8 @@
+# This file is a template - it must be renamed to apitoken.py and all fields filled out with the proper information.
+#Discord token for bot account
+token = ''
+#Twitch Client-ID and Client Secret keys, used for Twitch API calls
+clientid = ''
+clientsecret = ''
+#twitch header with token - Automatically set with the clientid provided above, no editing needed.
+twitchheader = {"Client-ID": clientid}
